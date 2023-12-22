8f06783eb87e82715d0d659624f14f8251d7104d
roninmaster21 <154482296+roninmaster21@users.noreply.github.com>
2023-12-21 20:47:02 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rfunctions',
+    version='0.0.1',
+    author='roninmaster21',
+    author_email='',
+    description='Use common rlua functions and libraries in Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/roninmaster21/rfunctions',
+    project_urls = {},
+    license='MIT',
+    packages=['rfunctions'],
+    install_requires=['requests'],
+)
