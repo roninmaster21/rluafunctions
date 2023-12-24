38aed181e4774f1a1387ebb87fefe98795fb8529
Ronin <154482296+roninmaster21@users.noreply.github.com>
2023-12-23 22:29:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='rluafunctions',
-    version='0.0.11',
+    version='0.1.0',
     author='roninmaster21',
     author_email='',
     description='Use common rlua functions and libraries in Python',
