9045a9d97144633474f31ef6f0a8a41c1cf6218e
Ronin <154482296+roninmaster21@users.noreply.github.com>
2023-12-23 20:56:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='rfunctions',
+    name='rluafunctions',
     version='0.0.2',
     author='roninmaster21',
     author_email='',
